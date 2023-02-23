f7f450e4c61e19fc180f86e3c4430908f7a3cbf3
sang ah <kr.sangah@gmail.com>
2023-02-23 11:30:20 +0900

    230223

--- js/main.js
+++ js/main.js
@@ -7,6 +7,49 @@ const deg = 360 / len;
 
 console.log(articleArr);
 
+const names = ["cardio", "groove", "happy", "light", "lily", "limes", "pop", "swing"]
+
 for (let i = 0; i < len; i++) {
   articleArr[i].style.transform = `rotate(${deg * i}deg) translateY(-100vh)`;
+
+  // 사진 부분 일괄 적용
+  const pic = articleArr[i].querySelector(".pic");
+  pic.style.backgroundImage = `url("../img/${names[i]}.jpg")`;
 }
+
+// Prev, Next 버튼 액션 처리
+const prev = document.querySelector(".btnPrev");
+const next = document.querySelector(".btnNext");
+
+let num = 0; // 회전 각도 조절용
+let active = 0; // 활성화 패널 위치 기억용
+
+prev.addEventListener("click", function () {
+  frame.style.transform = `rotate(${deg * ++num}deg)`;
+
+  if (active === 0) {
+    active = len - 1;
+  } else {
+    active--;
+  }
+
+  for (let el of articleArr) {
+    el.classList.remove("on");
+  }
+  articleArr[active].classList.add("on");
+});
+
+next.addEventListener("click", function () {
+  frame.style.transform = `rotate(${deg * --num}deg)`;
+
+  if (active === len - 1) {
+    active = 0;
+  } else {
+    active++;
+  }
+
+  for (let el of articleArr) {
+    el.classList.remove("on");
+  }
+  articleArr[active].classList.add("on");
+});
